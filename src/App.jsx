221ffa69b488ce69fdb0354d74b22bede026be81
iambasilp <basilxg321@gmail.com>
2023-01-29 17:05:00 +0530

    File and Folder structure initalized

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/system";
+import "./App.css";
+import {
+  ChannelDetails,
+  Feed,
+  Navbar,
+  SearchFeed,
+  VideoDetails,
+} from "./components";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000", color:"white"}}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetails />} />
+          <Route path="/channel/:id" element={<ChannelDetails />} />
+          <Route path="/search/:id" element={<SearchFeed />} />
+          {/* above we write /video/:id this is react router URL PARAMETERS this show specific component --iambasilp*/ }
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import ChannelDetails from './ChannelDetails'
+import Feed from './Feed'
+import Navbar from './Navbar'
+import SearchFeed from './SearchFeed'
+import VideoDetails from './VideoDetails'
+
+export {
+    ChannelDetails,
+    Feed,
+    Navbar,
+    SearchFeed,
+    VideoDetails
+}
